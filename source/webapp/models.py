8c18c1c3af85735ce7bc2120b742f28f74a0b017
Daniil <dshwets@gmail.com>
2020-10-25 00:07:21 +0600

    Сделан первый этап, добавленны модели для Фото и Избранного.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo_img = models.ImageField(upload_to='photos', verbose_name='Картинка')
+    signature = models.CharField(max_length=200, verbose_name='Подпись')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    author = models.ForeignKey(get_user_model(), verbose_name='Автор', related_name='author_photos',
+                               on_delete=models.CASCADE)
+
+
+class Favorite(models.Model):
+    photo = models.ForeignKey(Photo,  verbose_name='Фото', related_name='favorite_photo', on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), related_name='author_favorites',
+                               verbose_name='Автор', on_delete=models.CASCADE)
